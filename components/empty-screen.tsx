2b03ee54d1ee2dab98e8ce8fa5cff7390c78d29b
Sam Schneider <sam.ak.schneider@gmail.com>
2024-08-21 19:49:36 +0800

    Update color scheme to match makethejump scheme.

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -8,16 +8,17 @@ export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-        <h1 className="text-lg font-semibold">The Cat Cafe</h1>
+        <h1 className="text-2xl font-semibold">Hi there!</h1>
         <p className="leading-normal text-muted-foreground">
-          Hi, I'm an intelligent chatbot built by{' '}
+          I'm an intelligent chatbot built by{' '}
           {process.env.NEXT_PUBLIC_AUTHOR_NAME}.{' '}
+          I pretend to be a customer service agent for a fictional cat café.
         </p>
-        <p className="leading-normal text-muted-foreground">I can currently</p>
+
+        <h2 className="mt-6 text-md font-semibold">I can currently</h2>
         <ul className="list-disc list-inside text-muted-foreground">
           <li>Answer general questions about cats</li>
-          <li>Provide cat facts</li>
-          <li>Chat with you about cats available for adoption</li>
+          <li>Tell you about cats available for adoption</li>
         </ul>
       </div>
     </div>
--- components/header.tsx
+++ components/header.tsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-sky-600 bg-gradient-to-b from-background/10 via-background/10 to-background/20 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex items-center justify-center w-full px-4 py-6 border-b shrink-0 bg-[#e67e22] bg-gradient-to-b from-background/10 via-background/10 to-background/20 backdrop-blur-xl">
       <div className="flex items-center">
         <React.Suspense
           fallback={<div className="flex-1 overflow-auto" />}
         ></React.Suspense>
       </div>
-      <div className="flex items-center justify-end space-x-2"></div>
+      <div className="flex items-center justify-center space-x-2"><img src="/catbot-logo-orange.svg"/></div>
     </header>
   )
 }
